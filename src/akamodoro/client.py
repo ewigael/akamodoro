dd59959e55d3d43771899eb91ac401bb5a2fffc8
ewigael <311100398+ewigael@users.noreply.github.com>
2026-08-01 17:28:57 +0200

    functionnal gui

--- src/akamodoro/client.py
+++ src/akamodoro/client.py
@@ -15,46 +15,67 @@ BTN = {
 	"expand_x" : True,
 }
 
-def run_applet():
+def gen_powerline(
+	content, background="white", color="black", left="", right="", bar_bg="black"
+):
+	"""
+	Generates a powerline style string
+	"""
+	return (
+		f'<span background="{bar_bg}" color="{background}">{left}</span>'
+		+ f'<span background="{background}" color="{color}"> {content} </span>'
+		+ f'<span background="{bar_bg}" color="{background}">{right}</span>'
+	)
+
+def akamodoro_sendcmd(cmd):
+	sock = socket.socket(socket.AF_UNIX)
+
+	sock.connect(SOCKET_PATH)
+	sock.sendall(f"{cmd}\n".encode())
+
+	return sock.recv(1024).decode("utf-8").strip()
+
+
+def run_applet(x, y):
+	window_size = (368, 324)
 	layout = [
 		[TITLE],
-		[sg.Button("Re-START", **BTN)],
+		[sg.Button("Re-START", key="start", **BTN)],
 		[
-			sg.Button("Short Brk", **BTN),
-			sg.Button("Long Brk", **BTN)
+			sg.Button("Short Brk", key="short_brk", **BTN),
+			sg.Button("Long Brk", key="long_brk", **BTN)
 		],
-		[sg.Button("Work", **BTN)],
+		[sg.Button("Next", key="next", **BTN)],
 		[
-			sg.Button("Stop", **BTN),
-			sg.Button("Resume", **BTN)
+			sg.Button("Stop", key="stop", **BTN),
+			sg.Button("Resume", key="resume", **BTN)
 		]
 	]
-
-	mouse_pos = sg.Window.mouse_location()
 
 	window = sg.Window(
 		'AKAMODORO - applet',
 		layout,
 		font=("JetBrainsMonoNL Nerd Font Mono", 10),
-		location=(mouse_pos[0] - 100, mouse_pos[1] + 30)
+		finalize=True,
+		location=(x - (window_size[0] // 2), y + 30)
 	)
 	event, values = window.read()
+
 	window.close()
-	
-	print(event)
-	print(values)
+	if event != "resume":
+		akamodoro_sendcmd(event)
+
 
 def main():
 	argv = sys.argv
 	if len(argv) <= 1:
 		print("please add cmd")
 		return
-	# run_applet()
 
-	sock = socket.socket(socket.AF_UNIX)
-	sock.connect(SOCKET_PATH)
-	sock.sendall(f"{argv[1]}\n".encode())
-	print(sock.recv(1024).decode("utf-8").strip())
+	print(akamodoro_sendcmd(argv[1]))
+
+	if len(argv) == 4:
+		run_applet(int(argv[2]), int(argv[3]))
 
 if __name__ == '__main__':
     main()
